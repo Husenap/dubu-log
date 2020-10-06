fb0cdfd9c44a1759f46a766d40c71c3b65eeaadd
Hussein Taher <hussein_taher@outlook.com>
2020-10-06 23:00:04 +0200

    added includes

--- dubu_log/src/dubu_log/dubu_log.h
+++ dubu_log/src/dubu_log/dubu_log.h
@@ -1,5 +1,14 @@
 #pragma once
 
+#include <array>
+#include <cstdint>
+#include <filesystem>
+#include <fstream>
+#include <string>
+
+#include <fmt/format.h>
+#include <fmt/ostream.h>
+
 #include "dubu_log/logger/ConsoleLogger.h"
 #include "dubu_log/logger/FileLogger.h"
 #include "dubu_log/logger/ILogger.h"
